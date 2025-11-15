a086942893879102166c733a451c84c3cc47a7a3
Vedant Boddula <vedantboddula9860@gmail.com>
2025-11-16 01:18:45 +0530

    done now

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,12 +13,11 @@ dotenv.config();
 
 
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-/////////////////////////////////////////////////////////////
+
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
@@ -34,7 +33,7 @@ if(process.env.NODE_ENV=="production"){
 }
 
 app.get("*",(req,res) =>{
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
 })
 
 server.listen(PORT, () => {
