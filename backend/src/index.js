eea17cde382c561af31b5ddf4806bc702a8db436
Vedant Boddula <vedantboddula9860@gmail.com>
2025-11-16 14:30:28 +0530

    fixed now

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(express.json());
+
 
 app.use(cookieParser());
 app.use(cors({
@@ -32,8 +32,8 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 }
 
-app.get("*",(req,res) =>{
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+app.use((req,res) =>{
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
