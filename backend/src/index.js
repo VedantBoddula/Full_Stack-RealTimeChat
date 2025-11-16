37cdefe878dbd1f3c30134481987e3337bbf92d7
Vedant Boddula <vedantboddula9860@gmail.com>
2025-11-16 16:38:47 +0530

    changes made in uploading photos

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
 
 app.use(cookieParser());
